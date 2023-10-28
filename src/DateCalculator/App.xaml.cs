c8ba4c26dd509152c5698069a243e144f25eae32
Hikari Liou <complex0214@gmail.com>
2023-10-28 18:27:20 +0800

    初始化專案

---
+++ src/DateCalculator/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DateCalculator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/DateCalculator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DateCalculator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
